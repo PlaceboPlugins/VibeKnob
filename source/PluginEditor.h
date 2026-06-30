6af3c22c7cbd6098a1918dc2d893643fef22c0ba
PlaceboPlugins <amanda.klinthall@gmail.com>
2026-06-30 16:40:21 +0200

    Add particle struct and timer to PluginEditor
    Refactor PluginEditor class to include particle struct and timer functionality.

--- source/PluginEditor.h
+++ source/PluginEditor.h
@@ -1,11 +1,9 @@
 #pragma once
 
-#include <juce_gui_basics/juce_gui_basics.h>
+#include "PluginProcessor.h"
 
-// This tells the compiler "Hey, this class exists, don't panic!"
-class PamplejuceAudioProcessor; 
-
-class PamplejuceAudioProcessorEditor  : public juce::AudioProcessorEditor
+class PamplejuceAudioProcessorEditor  : public juce::AudioProcessorEditor,
+                                        private juce::Timer
 {
 public:
     explicit PamplejuceAudioProcessorEditor (PamplejuceAudioProcessor&);
@@ -15,12 +13,30 @@ public:
     void resized() override;
 
 private:
+    struct Particle
+    {
+        float x = 0.0f;
+        float y = 0.0f;
+        float speedX = 0.0f;
+        float speedY = 0.0f;
+        float scale = 1.0f;
+        float rotation = 0.0f;
+        float rotationSpeed = 0.0f;
+        bool isUnicorn = true; // true = image, false = procedural star!
+    };
+
+    void timerCallback() override;
+
+    PamplejuceAudioProcessor& processorRef;
+
     juce::Slider vibeKnob;
     juce::Label titleLabel;
     juce::Label statusLabel;
+
     float currentVibeValue = 0.0f;
 
-    PamplejuceAudioProcessor& processorRef;
+    std::vector<Particle> particles;
+    juce::Random random;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PamplejuceAudioProcessorEditor)
 };
